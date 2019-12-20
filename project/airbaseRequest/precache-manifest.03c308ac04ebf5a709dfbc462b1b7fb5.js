6b74227f314d16c25fd5d797590d40c743023625
Tilak Ranjan Mohanta <tilak.r.mohanta@gmail.com>
2019-12-20 19:27:15 +0530

    airbase
    demo project

---
+++ project/airbaseRequest/precache-manifest.03c308ac04ebf5a709dfbc462b1b7fb5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0761732c94bbbf000319cbafada30e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6b66d46ee258e72337f",
+    "url": "/static/css/main.d47aa802.chunk.css"
+  },
+  {
+    "revision": "851cd8f81c572d4e32e6",
+    "url": "/static/js/2.d88c9910.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d88c9910.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6b66d46ee258e72337f",
+    "url": "/static/js/main.ea6d31ff.chunk.js"
+  },
+  {
+    "revision": "fd52be45efdf33c307ab",
+    "url": "/static/js/runtime-main.48cf244d.js"
+  },
+  {
+    "revision": "6caeb41ab6f1b0d0f4b2b1304e01755d",
+    "url": "/static/media/Airbase_Logo.6caeb41a.svg"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/static/media/FontsFree-Net-SFProDisplay-Regular.d09549c1.ttf"
+  },
+  {
+    "revision": "c722dbbc36fd8931fe27d97b9de537a3",
+    "url": "/static/media/check.c722dbbc.svg"
+  },
+  {
+    "revision": "c95f57b25f47a79fe30a03229a08321d",
+    "url": "/static/media/file.c95f57b2.svg"
+  },
+  {
+    "revision": "370e89bad40c8cab22c58e8a1b6bb80b",
+    "url": "/static/media/lock.370e89ba.svg"
+  },
+  {
+    "revision": "7aa3516fed05bcd0db79cb161c28634e",
+    "url": "/static/media/unCheck.7aa3516f.svg"
+  }
+]);
